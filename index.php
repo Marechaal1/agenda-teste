59692a7ef6f84af1556260bb764e6c39f3815e75
Gunnar Vingren Alexandre da Silva Santos <gunnar.santos@unimedmaceio.local>
2023-08-11 15:48:56 -0300

    UP-01

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
-    include_once("./templates/header.php");
+include_once("./templates/header.php");
 ?>
- 
+
 <div class="container">
-    <?php if(isset($printMsg) && $printMsg != ''): ?>
+    <?php if (isset($printMsg) && $printMsg != '') : ?>
         <p id="msg"> <?= $printMsg ?></p>
-        <?php endif; ?>
-        <h1 id="main-title">Agenda da massa regueira </h1>
-        <?php if(count($contacts) > 0): ?>
-            <table class="table table-striped table-dark">
+    <?php endif; ?>
+    <h1 id="main-title">Agenda da massa regueira </h1>
+    <?php if (count($contacts) > 0) : ?>
+        <table class="table table-striped table-dark">
             <thead>
                 <tr>
                     <th scope="col">ID</th>
@@ -17,30 +17,26 @@
                     <th scope="col">Opções</th>
                 </tr>
             </thead>
-            <tbody>
                 <?php foreach ($contacts as $contact) : ?>
-                        <tbody>
-                            <tr>
-                                <th scope="row"><?= $contact["id"] ?></th>
-                                <td scope="row"><?= $contact["nameContact"] ?></td>
-                                <td scope="row"><?= $contact["phoneContact"] ?></td>
-                                <td class="actions">
-                                    <a href="<?= $BASE_URL ?>show.php?id=<?= $contact["id"] ?>"><i class="fas fa-eye check-icon"></i></a>
-                                    <a href="<?= $BASE_URL ?>edit.php?id=<?= $contact["id"] ?>"><i class="far fa-edit edit-icon"></i></a>
-                                    <button type="submit" class="delete-btn"><i class="fas fa-times delete-icon"></i></button>
-                        </td>
-                            </tr>
-                        </tbody>
-                <?php endforeach; ?>
-                        </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
-        <?php else : ?>
-            <p id="empty-list-text">Ainda não há contatos na sua agenda, <a href="<?=$BASE_URL?>create.php">clique aqui para adicionar contatos</a>.</p>
-        <?php endif; ?>
+            <tbody>
+                <tr>
+                    <th scope="row"><?= $contact["id"] ?></th>
+                    <td scope="row"><?= $contact["nameContact"] ?></td>
+                    <td scope="row"><?= $contact["phoneContact"] ?></td>
+                    <td class="actions">
+                        <a href="<?= $BASE_URL ?>show.php?id=<?= $contact["id"] ?>"><i class="fas fa-eye check-icon"></i></a>
+                        <a href="<?= $BASE_URL ?>edit.php?id=<?= $contact["id"] ?>"><i class="far fa-edit edit-icon"></i></a>
+                        <button type="submit" class="delete-btn"><i class="fas fa-times delete-icon"></i></button>
+                    </td>
+                </tr>
+            </tbody>
+        <?php endforeach; ?>
+        </table>
+    <?php else : ?>
+        <p id="empty-list-text">Ainda não há contatos na sua agenda, <a href="<?= $BASE_URL ?>create.php">clique aqui para adicionar contatos</a>.</p>
+    <?php endif; ?>
 </div>
 
 <?php
-    include_once("./templates/footer.php");
+include_once("./templates/footer.php");
 ?>
