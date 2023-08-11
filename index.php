58f94e4bf223185f57780e50b510387403570e75
Gunnar Vingren Alexandre da Silva Santos <gunnar.santos@unimedmaceio.local>
2023-08-11 17:08:06 -0300

    Att interface

--- index.php
+++ index.php
@@ -17,20 +17,20 @@ include_once("./templates/header.php");
                     <th scope="col">Opções</th>
                 </tr>
             </thead>
-                <?php foreach ($contacts as $contact) : ?>
-            <tbody>
-                <tr>
-                    <th scope="row"><?= $contact["id"] ?></th>
-                    <td scope="row"><?= $contact["nameContact"] ?></td>
-                    <td scope="row"><?= $contact["phoneContact"] ?></td>
-                    <td class="actions">
-                        <a href="<?= $BASE_URL ?>show.php?id=<?= $contact["id"] ?>"><i class="fas fa-eye check-icon"></i></a>
-                        <a href="<?= $BASE_URL ?>edit.php?id=<?= $contact["id"] ?>"><i class="far fa-edit edit-icon"></i></a>
-                        <button type="submit" class="delete-btn"><i class="fas fa-times delete-icon"></i></button>
-                    </td>
-                </tr>
-            </tbody>
-        <?php endforeach; ?>
+            <?php foreach ($contacts as $contact) : ?>
+                <tbody>
+                    <tr>
+                        <th scope="row"><?= $contact["id"] ?></th>
+                        <td scope="row"><?= $contact["nameContact"] ?></td>
+                        <td scope="row"><?= $contact["phoneContact"] ?></td>
+                        <td class="actions">
+                            <a href="<?= $BASE_URL ?>show.php?id=<?= $contact["id"] ?>"><i class="fas fa-eye check-icon" style="color: #1E7E34;"></i></a>
+                            <a href="<?= $BASE_URL ?>edit.php?id=<?= $contact["id"] ?>"><i class="far fa-edit edit-icon"></i></a>
+                            <button type="submit" class="delete-btn"> <i class="fa-solid fa-trash" style="color: #31df01"></i></button>
+                        </td>
+                    </tr>
+                </tbody>
+            <?php endforeach; ?>
         </table>
     <?php else : ?>
         <p id="empty-list-text">Ainda não há contatos na sua agenda, <a href="<?= $BASE_URL ?>create.php">clique aqui para adicionar contatos</a>.</p>
